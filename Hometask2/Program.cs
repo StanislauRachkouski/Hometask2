1caad006004f126153c02471ed2784fa8745d01b
StanislauRachkouski <StanislauRachkouski@users.noreply.github.com>
2020-12-03 12:03:25 +0300

    Добавьте файлы проекта.

---
+++ Hometask2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+
+
+namespace Hometask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********  Home Task 2/ Read lines from file csv  ********\n");
+            string path = @"D:\study\Garanin\Hometask2\hometask.csv";
+           
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    // Console.WriteLine(sr.ReadToEnd());
+                    string line;
+                    while ((line =sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+           
+            
+        }
+    }
+}
